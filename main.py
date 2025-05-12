1418679c168d5861da2790bea1f44fe97e94e491
bluegecko <kei09doya@gmail.com>
2025-05-12 10:09:40 +0900

    Set translucent background for the main widget
    Added the WA_TranslucentBackground attribute to the main widget's initialization. This change enhances the visual appearance by allowing background transparency, aligning with the frameless and bottom-hint window design.

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
         self.setWindowFlags(Qt.WindowType.WindowStaysOnBottomHint | Qt.WindowType.FramelessWindowHint)
 
         central_widget = QWidget()
